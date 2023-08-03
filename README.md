6d77161107421550605c2e4b63d05d606181b823
神代綺凛 <i@loli.best>
2023-08-03 14:50:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# unity-js-tools
+
+[![NPM version](https://img.shields.io/npm/v/@arkntools/unity-js-tools?style=flat-square)](https://www.npmjs.com/package/@arkntools/unity-js-tools)
+
+一些给 [unity-js](https://github.com/arkntools/unity-js) 用的工具方法，从 rust 编译为 wasm
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use astc_decode::{astc_decode, Footprint};
+use lz4_flex::decompress;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(js_name = decodeAstc)]
+pub fn decode_astc(
+    data: &[u8],
+    width: u32,
+    height: u32,
+    block_width: u32,
+    block_height: u32,
+) -> Result<Vec<u8>, JsError> {
+    let footprint = Footprint::new(block_width, block_height);
+    let mut result: Vec<u8> = vec![0; (width * height * 4) as usize];
+
+    let astc_result = astc_decode(data, width, height, footprint, |x, y, v4| {
+        let y = height - y - 1;
+        let ri = ((y * width + x) * 4) as usize;
+        for (i, v) in v4.iter().enumerate() {
+            result[ri + i] = *v;
+        }
+    });
+
+    match astc_result {
+        Ok(()) => Ok(result),
+        Err(e) => Err(JsError::new(&e.to_string())),
+    }
+}
+
+#[wasm_bindgen(js_name = decompressLz4)]
+pub fn decompress_lz4(data: &[u8], size: usize) -> Result<Vec<u8>, JsError> {
+    decompress(data, size).map_err(|e| JsError::new(&e.to_string()))
+}
